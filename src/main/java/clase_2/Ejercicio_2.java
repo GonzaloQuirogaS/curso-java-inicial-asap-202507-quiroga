63055ce3a35132f268ce01221b5698314e0b3c9b
Gonzalo Quiroga <gonzaloquirogasassu@gmail.com>
2025-07-24 16:40:56 -0300

    fix ejercicio 2 clase 2

--- src/main/java/clase_2/Ejercicio_2.java
+++ src/main/java/clase_2/Ejercicio_2.java
@@ -3,7 +3,6 @@ package clase_2;
 import java.util.Scanner;
 
 public class Ejercicio_2 {
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -12,25 +11,26 @@ public class Ejercicio_2 {
         System.out.print("Digite el segundo numero: ");
         int b = sc.nextInt();
 
-        if (amigos(a,b)){
-            System.out.println("Los numeros son amigos");
-        }else {
-            System.out.println("Los numeros no son amigos");
-        }
-    }
-
-    public static boolean amigos(int a, int b) {
-        return totalDivisores(a) == b && totalDivisores(b) == a;
-    }
-
-    public static int totalDivisores(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) {
-                sum += i;
+        if (a >= 0 && b >= 0) {
+            int sumaA = 0;
+            for (int i = 1; i < a; i++) {
+                if (a % i == 0) {
+                    sumaA += i;
+                }
             }
+            int sumaB = 0;
+            for (int i = 1; i < b; i++) {
+                if (b % i == 0) {
+                    sumaB += i;
+                }
+            }
+            if (sumaA == b && sumaB == a) {
+                System.out.println(a + " y " + b + " son números amigos.");
+            } else {
+                System.out.println(a + " y " + b + " no son números amigos.");
+            }
+        } else {
+            System.out.println("Los digitos ingresados no pueden ser negativos");
         }
-        return sum;
     }
-
 }
